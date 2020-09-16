3d0f5d37d1cae5f0de4745df85bff4e9943079f8
DSmith12345 <dcs2344@vt.edu>
2020-09-16 09:25:56 -0400

    Variable name and comment changes

--- DB_Upload.py
+++ DB_Upload.py
@@ -1,8 +1,6 @@
 import psycopg2
 import sqlalchemy
 import pandas as pd
-# Note that this will import cbdata function when it is functional, replacing countypov which is for testing.
-from povertycode import countypov
 from CB_Data import cbdata
 
 # TableName is the name of the table that will be created in the database.
@@ -17,7 +15,7 @@ def upload(TableName, Dataframe ,DB):
     # Read in dataset (pandas format).
     # Ideally df would be equal to countypov(StateNum) which is a dataframe.
     #df = pd.read_csv(location)
-    df = Table
+    df = Dataframe
 
     # Connects to data base.
     engine = sqlalchemy.create_engine(db)
