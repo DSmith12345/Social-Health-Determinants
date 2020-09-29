ff5b1845a9bda6ea107a6bfd74590bcdb9db37c1
DSmith12345 <dcs2344@vt.edu>
2020-09-28 22:13:16 -0400

    Added changes for dictionary input

--- povertycode.py
+++ povertycode.py
@@ -1,10 +1,11 @@
 # Author Daniel Smith 
-# Edited: 9/18/20
+# Edited: 9/28/20
 # Pulls poverty data from Census Bureau and exports to database.
 
 # Imports.
 import pandas as pd
 import censusdata
+import configparser
 import urllib
 from sqlalchemy import create_engine
 from CB_Data import cbdata
@@ -17,22 +18,27 @@ pd.set_option('display.precision', 2)
 # DP05_0001E is total population.
 # DP03_0119PE is percent of poverty.
 
+#
+with open('server.txt', 'r') as file:
+    server = file.read()
+
+
 # Virginia data call.
-VACounty = cbdata(51, 'county', 'DP05_0001E', 'poverty')
+VACounty = cbdata(51, 'county', {'population':'DP05_0001E', 'poverty':'DP03_0119PE'})
 # West Virginia data call.
-WVCounty = cbdata(54, 'county', 'DP05_0001E', 'poverty')
+WVCounty = cbdata(54, 'county', {'population':'DP05_0001E', 'poverty':'DP03_0119PE'})
 # Combines to one dataframe.
 countypoverty = pd.concat([VACounty, WVCounty])
 
 # Sends to data to database.
-upload('poverty_county', countypoverty, "postgresql://postgres:Password@localhost/SDoH_DB")
+upload('poverty_county', countypoverty, server)
 
 # Virginia data call.
-VATract = cbdata(51, 'tract', 'B17001_002E', 'poverty')
+VATract = cbdata(51, 'tract',  {'population':'B01001_001E', 'poverty':'B17001_002E'})
 # West Virginia data call.
-WVTract = cbdata(54, 'tract', 'B17001_002E', 'poverty')
+WVTract = cbdata(54, 'tract',  {'population':'B01001_001E', 'poverty':'B17001_002E'})
 # Combines to one dataframe.
 tractpoverty = pd.concat([VATract, WVTract])
 
 # Exports data to database.
-upload('poverty_tract', tractpoverty, "postgresql://postgres:Password@localhost/SDoH_DB")
+upload('poverty_tract', tractpoverty, server)
