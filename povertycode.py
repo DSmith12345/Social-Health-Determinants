780a7a26217d2f8287734879cf60861de7158d01
DSmith12345 <dcs2344@vt.edu>
2020-10-17 15:45:30 -0400

    Updated variable input

--- povertycode.py
+++ povertycode.py
@@ -22,11 +22,15 @@ pd.set_option('display.precision', 2)
 with open('server.txt', 'r') as file:
     server = file.read()
 
+# Variables to be pulled for counties.
+var_county =  {'population':'DP05_0001E', 'poverty_percent':'DP03_0119PE'}
+# Variables to be pulled for tracts.
+var_tract =  {'population':'B01001_001E', 'poverty_count':'B17001_002E'}
 
 # Virginia data call.
-VACounty = cbdata(51, 'county', {'population':'DP05_0001E', 'poverty':'DP03_0119PE'})
+VACounty = cbdata(51, 'county', var_county)
 # West Virginia data call.
-WVCounty = cbdata(54, 'county', {'population':'DP05_0001E', 'poverty':'DP03_0119PE'})
+WVCounty = cbdata(54, 'county', var_county)
 # Combines to one dataframe.
 countypoverty = pd.concat([VACounty, WVCounty])
 
@@ -34,9 +38,9 @@ countypoverty = pd.concat([VACounty, WVCounty])
 upload('poverty_county', countypoverty, server)
 
 # Virginia data call.
-VATract = cbdata(51, 'tract',  {'population':'B01001_001E', 'poverty':'B17001_002E'})
+VATract = cbdata(51, 'tract',  var_tract)
 # West Virginia data call.
-WVTract = cbdata(54, 'tract',  {'population':'B01001_001E', 'poverty':'B17001_002E'})
+WVTract = cbdata(54, 'tract', var_tract)
 # Combines to one dataframe.
 tractpoverty = pd.concat([VATract, WVTract])
 
