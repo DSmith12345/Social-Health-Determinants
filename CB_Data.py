ef7aebff3fe3bb1cbd1a1345b449068ed537a241
DSmith12345 <dcs2344@vt.edu>
2020-10-21 08:56:19 -0400

    Added year input to CB_Data

--- CB_Data.py
+++ CB_Data.py
@@ -6,18 +6,20 @@ pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.precision', 2)
 
 # StateNum is the number of the state.
+# Year of the data to be pulled.
 # Level is the granularity, e.g. county or tract.
 # Variables is a dictionary that takes in the names and variables that will be pulled.
 # Example: {'population':'DP05_0001E', 'poverty_percent':'DP03_0119PE'}
-def cbdata (StateNum, Level, Variables):
+def cbdata (StateNum, Year, Level, Variables):
         num = str(StateNum)
+        y = int(Year)
         level = Level
         dictionary = Variables
         codes = list(dictionary.values())
         names = list(dictionary.keys())
         
         if (level == "county" or level == "County"):
-            data=censusdata.download('acs5', 2018, censusdata.censusgeo([('state', num),('county', '*')]), codes, tabletype='profile')
+            data=censusdata.download('acs5', y, censusdata.censusgeo([('state', num),('county', '*')]), codes, tabletype='profile')
             
             niter = len(codes)
             for i in range(niter):
@@ -62,7 +64,7 @@ def cbdata (StateNum, Level, Variables):
             final = result.drop(columns=['longform'])
             
         elif (level == "tract" or level == "Tract"):
-            data=censusdata.download('acs5', 2018,
+            data=censusdata.download('acs5', y,
             censusdata.censusgeo([('state', num),('county', '*'),('tract','*')]),codes)
             
             niter = len(codes)
--- povertycode.py
+++ povertycode.py
@@ -26,11 +26,13 @@ with open('server.txt', 'r') as file:
 var_county =  {'population':'DP05_0001E', 'poverty_percent':'DP03_0119PE'}
 # Variables to be pulled for tracts.
 var_tract =  {'population':'B01001_001E', 'poverty_count':'B17001_002E'}
+# Year of data.
+Year = 2018
 
 # Virginia data call.
-VACounty = cbdata(51, 'county', var_county)
+VACounty = cbdata(51, Year,'county', var_county)
 # West Virginia data call.
-WVCounty = cbdata(54, 'county', var_county)
+WVCounty = cbdata(54, Year,'county', var_county)
 # Combines to one dataframe.
 countypoverty = pd.concat([VACounty, WVCounty])
 
@@ -38,9 +40,9 @@ countypoverty = pd.concat([VACounty, WVCounty])
 upload('poverty_county', countypoverty, server)
 
 # Virginia data call.
-VATract = cbdata(51, 'tract',  var_tract)
+VATract = cbdata(51, Year, 'tract',  var_tract)
 # West Virginia data call.
-WVTract = cbdata(54, 'tract', var_tract)
+WVTract = cbdata(54, Year, 'tract', var_tract)
 # Combines to one dataframe.
 tractpoverty = pd.concat([VATract, WVTract])
 
