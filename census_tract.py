82f101eaf96769faa8e3d88ca7a5a2b89bc67c37
Humphrey, Lester E <lehumphrey@carilionclinic.org>
2021-03-17 21:12:11 -0400

    cleaned up unneeded files

--- census_tract.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-#TODO identify your remote sources
-
-#TODO pull those sources into dataframes
-
-#TODO start joining them shaping them and then staging at as an output dataframe
